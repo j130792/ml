4111c6b1e64483fb2ed7e9abd7484d687cb4e2cc
James Jackaman <james@jackaman.net>
2020-09-14 14:48:35 -0230

    Add function RemoveBoringClasses which will remove all periods of class 7 and leave a single instance, and then remove all periods of class 6 except a single instance etc. up to a given class specified by the user as 7-jump

--- prune_era.py
+++ prune_era.py
@@ -112,8 +112,115 @@ def RemoveBoringPeriod(bp=2):
     np.save('data/TdmodelFullP%s.npy' % bp, PrunedTdmodel)
     
     return PrunedVisobs
-                    
 
+#A function which converts a visability into a class
+def ClassConverter(vis):
+    if vis>=5000:
+        Class = 7
+    elif vis>=3000:
+        Class = 6
+    elif vis>=1500:
+        Class = 5
+    elif vis>=800:
+        Class = 4
+    elif vis>=600:
+        Class = 3
+    elif vis>=350:
+        Class = 2
+    elif vis>=150:
+        Class = 1
+    else:
+        Class = 0
+    return Class
+
+#Function will remove instances where we remain in class 7 for more than 1 hour.
+#It will also remove all but one instance of adjust classes up to class 7-jump
+def RemoveBoringClasses(jump=0):
+    #Check user input is sensible
+    if jump<0:
+        raise ValueError('jump must be zero or greater for the algorithm to make sense')
+    elif jump>6:
+        raise ValueError('jump is set too high, there are not enough classes for algorithm to make sense')
+    if jump!=int(jump):
+        raise ValueError('jump must be an integer')
+
+    bp = 2
+    #Run algorithm marking indices for deletion
+    VisLength = np.size(Visobs)
+    DeletionIndex = []
+    for i in range(VisLength-bp+1):
+        LocalVisobs = Visobs[i:i+bp]
+        if min(LocalVisobs)>=5000:
+            DeletionIndex.append(np.arange(i+1,i+bp)) #Don't mark first entry for deletion
+
+    #Define a function to remove all but required values from deletion index
+    def ReducedIndex(DeletionIndex):
+
+        unique = np.unique(DeletionIndex)
+        SubDeletionIndex = []
+        
+        for i in range(len(unique)-1):
+            if unique[i]+1==unique[i+1]:
+                SubDeletionIndex.append(i)
+
+        reducedindex = np.delete(unique,SubDeletionIndex)
+                
+        return reducedindex
+            
+    ##Recursively check the adjacent class for repeated entries
+    #List classes marked for pruning
+    Classes = [i+6-jump for i in range(jump,0,-1)]
+    for Class in Classes:
+        RIndex = ReducedIndex(DeletionIndex)
+        for i in RIndex:
+            ##Check if two adjacent values to deletion class also need to be deleted
+            #Downwind check and mark
+            counter = 1
+            while 1>0:
+                try:
+                    minus1 = ClassConverter(Visobs[i-counter])
+                    minus2 = ClassConverter(Visobs[i-counter-1])
+                except:
+                    break
+                if (minus1==Class and minus2==Class):
+                    DeletionIndex.append([i-counter])
+                elif (minus1>Class or minus2>Class):
+                    do = -1
+                else:
+                    break
+                counter = counter + 1
+            #Upwind check and mark
+            counter = 1
+            while 1>0:
+                try:
+                    plus1 = ClassConverter(Visobs[i+counter])
+                    plus2 = ClassConverter(Visobs[i+counter+1])
+                except:
+                    break
+                if (plus1==Class and plus2==Class):
+                    DeletionIndex.append([i+counter])
+                elif (minus1>Class or minus2>Class):
+                    do = -1
+                else:
+                    break
+                counter = counter + 1
+
+    #Remove entries primed for deletion
+    #(indexes may be marked for deletion multiple times but can only be deleted once)
+    PrunedVisobs = np.delete(Visobs,DeletionIndex)
+    PrunedTobs = np.delete(Tobs,DeletionIndex)
+    PrunedTmodel = PruneModel(Tmodel,DeletionIndex)
+    PrunedTdobs = np.delete(Tdobs,DeletionIndex)
+    PrunedTdmodel = PruneModel(Tdmodel,DeletionIndex)
+
+    #Save data
+    np.save('data/VisobsFullC%s.npy' % bp, PrunedVisobs)
+    np.save('data/TobsFullC%s.npy' % bp, PrunedTobs)
+    np.save('data/TmodelFullC%s.npy' % bp, PrunedTmodel)
+    np.save('data/TdobsFullC%s.npy' % bp, PrunedTdobs)
+    np.save('data/TdmodelFullC%s.npy' % bp, PrunedTdmodel)
+
+    return PrunedVisobs
 
 if __name__=="__main__":
     np.set_printoptions(threshold=maxsize) #Allows us to print entire array
@@ -122,4 +229,7 @@ if __name__=="__main__":
     #print(out)
     
     out = RemoveBoringPeriod(2)
+    # print(out)
+
+    out = RemoveBoringClasses(1)
     # print(out)
